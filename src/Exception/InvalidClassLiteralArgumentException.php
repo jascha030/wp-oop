1358685b8c578e5876a2591277e8e15aec459a7b
Jascha van Aalst <mr.jascha@gmail.com>
2020-07-08 02:23:22 +0200

    Add exceptions

---
+++ src/Exception/InvalidClassLiteralArgumentException.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Jascha030\WP\OOPOR\Exception;
+
+/**
+ * Class InvalidClassLiteralArgumentException
+ *
+ * @package Jascha030\WP\OOPOR\Exception
+ * @author Jascha van Aalst <contact@jaschavanaalst.nl>
+ */
+final class InvalidClassLiteralArgumentException extends InvalidClassLiteralException
+{
+    private const PREPEND_STRING = 'Invalid argument%s: ';
+
+    public function __construct(
+        string $argumentName = null,
+        string $providedClass = null,
+        $implements = null,
+        string $message = null
+    ) {
+        parent::__construct($message ? $this->createArgumentMessage($argumentName, $providedClass, $implements) : null);
+    }
+
+
+    /**
+     * @param string|null $argumentName
+     * @param string|null $providedClass
+     * @param string|array|null $implements
+     *
+     * @return string
+     */
+    private function createArgumentMessage(
+        string $argumentName = null,
+        string $providedClass = null,
+        string $implements = null
+    ): string {
+        $append  = sprintf(self::PREPEND_STRING, ! $argumentName ? '' : $argumentName);
+        $classes = is_array($implements) ? implode(',', $implements) : $implements;
+
+        return $append . $this->createMessage($providedClass, $classes, is_array($implements));
+    }
+}
---
+++ src/Exception/InvalidClassLiteralException.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Jascha030\WP\OOPOR\Exception;
+
+use Psr\Container\ContainerExceptionInterface;
+
+/**
+ * Class InvalidArgumentImplementationException
+ *
+ * Throwable for Class string literal arguments that don't implement their expected interface
+ *
+ * @package Jascha030\WP\OOPOR\Exception
+ * @author Jascha van Aalst <contact@jaschavanaalst.nl>
+ */
+class InvalidClassLiteralException extends \Exception implements ContainerExceptionInterface
+{
+    private const MESSAGE_TEMPLATE = '%s does not implement %s.';
+
+    /**
+     * InvalidClassLiteralException constructor.
+     *
+     * @param string $providedClass
+     * @param string|null $implements
+     * @param bool $multiple
+     * @param string|null $message
+     */
+    public function __construct(
+        string $providedClass = null,
+        string $implements = null,
+        bool $multiple = false,
+        string $message = null
+    ) {
+        parent::__construct(! $message ? $this->createMessage($providedClass, $implements, $multiple) : $message);
+    }
+
+    /**
+     * @param string $providedClass
+     * @param string|null $implements
+     * @param bool $multiple
+     *
+     * @return string
+     */
+    final public function createMessage(
+        string $providedClass = '',
+        string $implements = null,
+        bool $multiple = false
+    ): string {
+        $prepend = ! $providedClass ? 'Class ' : sprintf('Class: %s', $providedClass);
+        $append  = 'interface.';
+
+        if ($implements && $multiple) {
+            $append = $multiple ? 'interface: %s.' : 'one of the following interfaces: %s.';
+            $append = sprintf($append, $implements);
+        }
+
+        return sprintf(self::MESSAGE_TEMPLATE, $prepend, $append);
+    }
+}
