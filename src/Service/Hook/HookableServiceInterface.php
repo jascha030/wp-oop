7005f8f84bea8eb6fa657b1e6939d98196a2576a
Jascha van Aalst <mr.jascha@gmail.com>
2020-07-08 02:24:02 +0200

    Require container in HookableServiceInterface constructor

--- src/Service/Hook/HookableServiceInterface.php
+++ src/Service/Hook/HookableServiceInterface.php
@@ -2,6 +2,15 @@
 
 namespace Jascha030\WP\OOPOR\Service\Hook;
 
+use Jascha030\WP\OOPOR\Container\Psr11\WpPluginApiContainerInteface;
+
+/**
+ * Interface HookableServiceInterface
+ *
+ * @package Jascha030\WP\OOPOR\Service\Hook
+ * @author Jascha van Aalst <contact@jaschavanaalst.nl>
+ */
 interface HookableServiceInterface
 {
+    public function __construct(WpPluginApiContainerInteface $container);
 }
